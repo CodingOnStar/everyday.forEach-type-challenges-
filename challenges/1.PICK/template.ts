043394b5cfb5d5575807a9b0d7a0fdca7b035e97
tencent_hanxujiang <hanxujiang@tencent.com>
2022-02-16 16:15:18 +0800

    east(1):pick

---
+++ challenges/1.PICK/template.ts
@@ -0,0 +1,15 @@
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+// extends对应element in todo， keyof对应ts的联合类型
+
+// function myPick(todo: Object, keys: string[]): Object {
+//   const obj = {};
+//   keys.forEach((element) => {
+//     if (element in todo) {
+//       obj[element] = todo[element];
+//     }
+//   });
+//   return obj;
+// }
